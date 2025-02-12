ef9016227150109e43407c3045621050e6805baf
freemanxliu <63280652+freemanxliu@users.noreply.github.com>
2025-02-12 22:51:45 +0800

    Create Bubbles.glsl

---
+++ Shaders/Bubbles.glsl
@@ -0,0 +1,21 @@
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = fragCoord/iResolution.xy;
+
+    // Time varying pixel color
+    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+
+    // Output to screen
+    fragColor = vec4(col,1.0);
+
+     vec3 outputColor = mix(BACKGROUND_COLOR, BUBBLE_COLOR, shapeFactor);
+    
+    // Output to screen
+    
+    vec3 color = mix(BACKGROUND_COLOR, BUBBLE_COLOR, shapeFactor);
+    
+    // Output to screen
+    fragColor = vec4(color, 1.0);
+    
+}
